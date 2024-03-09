a7190c7ff1d71837d0a2fae120a94ae728d6db80
RamiroRojasBociga <111482131+RamiroRojasBociga@users.noreply.github.com>
2024-03-09 13:59:04 -0500

    Se modifica la clase Inventario
    Se modifica la clase para agregarle funcionalidad

--- src/main/java/com/example/inventario/model/InventarioProductos.java
+++ src/main/java/com/example/inventario/model/InventarioProductos.java
@@ -1,32 +1,30 @@
 package com.example.inventario.model;
 
 import java.util.Comparator;
-import java.util.HashMap;
+import java.util.List;
 import java.util.TreeSet;
 
+import com.example.inventario.utils.Persistencia;
+
 public class InventarioProductos {
-    private HashMap<String, Producto> inventarioProductos;
-    private TreeSet<Producto> productosConCantidadBaja; // TreeSet para mantener los productos ordenados por cantidad en inventario
+    private TreeSet<Producto> productosConCantidadBaja;
 
-    private static final int CANTIDAD_MINIMA_REABASTECIMIENTO = 10; // Por ejemplo, podrías definirla como 10 unidades
-
+    private static final int CANTIDAD_MINIMA_REABASTECIMIENTO = 10;
 
     public InventarioProductos() {
-        this.inventarioProductos = new HashMap<>();
-        this.productosConCantidadBaja = new TreeSet<>(new ComparadorProductosPorCantidad()); // Usar un comparador para ordenar por cantidad en inventario
-
+        productosConCantidadBaja = new TreeSet<>(new ComparadorProductosPorCantidad());
+        cargarProductosDesdeArchivo();
     }
 
-    public void agregarProducto(Producto producto) {
-        inventarioProductos.put(producto.getCodigoUnico(), producto);
-        if (producto.getCantidadInventario() <= CANTIDAD_MINIMA_REABASTECIMIENTO) {
-            productosConCantidadBaja.add(producto);
+    private void cargarProductosDesdeArchivo() {
+        List<Producto> productos = Persistencia.cargarProductosDesdeArchivo();
+        for (Producto producto : productos) {
+            if (producto.getCantidadInventario() <= CANTIDAD_MINIMA_REABASTECIMIENTO) {
+                productosConCantidadBaja.add(producto);
+            }
         }
     }
 
-    public Producto obtenerProducto(String codigo) {
-        return inventarioProductos.get(codigo);
-    }
     private static class ComparadorProductosPorCantidad implements Comparator<Producto> {
         @Override
         public int compare(Producto p1, Producto p2) {
@@ -34,5 +32,5 @@ public class InventarioProductos {
         }
     }
 
-    // Otros métodos según sea necesario...
+    // Otros métodos relacionados con el inventario, como obtener productos, etc.
 }
