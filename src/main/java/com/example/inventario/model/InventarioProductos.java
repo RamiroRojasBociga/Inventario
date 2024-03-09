0f827e2ee3fd1a6d5a40bd82743e544ffad0db3a
RamiroRojasBociga <111482131+RamiroRojasBociga@users.noreply.github.com>
2024-03-09 14:30:54 -0500

    Se deja funcional clase inventario por consola
    Se imprime por consola los productos ordenados del inventario

--- src/main/java/com/example/inventario/model/InventarioProductos.java
+++ src/main/java/com/example/inventario/model/InventarioProductos.java
@@ -1,5 +1,6 @@
 package com.example.inventario.model;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.TreeSet;
@@ -7,6 +8,8 @@ import java.util.TreeSet;
 import com.example.inventario.utils.Persistencia;
 
 public class InventarioProductos {
+
+
     private TreeSet<Producto> productosConCantidadBaja;
 
     private static final int CANTIDAD_MINIMA_REABASTECIMIENTO = 10;
@@ -14,6 +17,7 @@ public class InventarioProductos {
     public InventarioProductos() {
         productosConCantidadBaja = new TreeSet<>(new ComparadorProductosPorCantidad());
         cargarProductosDesdeArchivo();
+        System.out.println("Productos cargados desde el archivo: " + productosConCantidadBaja);
     }
 
     private void cargarProductosDesdeArchivo() {
@@ -25,7 +29,13 @@ public class InventarioProductos {
         }
     }
 
-    private static class ComparadorProductosPorCantidad implements Comparator<Producto> {
+    public List<Producto> obtenerProductosOrdenadosPorCantidad() {
+        List<Producto> productosOrdenados = new ArrayList<>(productosConCantidadBaja);
+        productosOrdenados.sort(new ComparadorProductosPorCantidad());
+        return productosOrdenados;
+    }
+
+    public static class ComparadorProductosPorCantidad implements Comparator<Producto> {
         @Override
         public int compare(Producto p1, Producto p2) {
             return Integer.compare(p1.getCantidadInventario(), p2.getCantidadInventario());
