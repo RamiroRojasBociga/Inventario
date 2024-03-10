987a3196ff820b92ef96807c54f541fb79dfd8d4
RamiroRojasBociga <111482131+RamiroRojasBociga@users.noreply.github.com>
2024-03-09 22:56:17 -0500

    Actualizacion de estilos
    Se actualizan los estilos de los botones y las tablas

--- src/main/java/com/example/inventario/HelloApplication.java
+++ src/main/java/com/example/inventario/HelloApplication.java
@@ -17,8 +17,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("inicio-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 600, 450);
+        stage.setTitle("Tienda");
         stage.setScene(scene);
         stage.show();
     }
